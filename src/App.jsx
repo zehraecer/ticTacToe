e4cf2d5a26a2ccf4d9774e180d0fa044421bed9e
Zehra Ecer <zehraecer1996@hotmail.com>
2024-06-24 21:14:07 +0300

    header tasarımı  yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
+import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './components/Header'
-
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
+      <div className="container">
+
+        <Header />
+
+      </div>
+
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,24 @@ export const Header = () => {
 
     return (
         <>
+            <div className="header">
 
+                <div className="header-left">
+
+                    <img src="./x.svg" alt="" />
+                    <img src="./o.svg" alt="" />
+
+                </div>
+
+                <div className="header-middle">
+                    <img src="./grey-x.svg" alt="" />
+                    <h3>TURN</h3>
+                </div>
+
+                <div className="header-right">
+                    <img src="./restart.svg" alt="" />
+                </div>
+            </div>
         </>
     )
 }
